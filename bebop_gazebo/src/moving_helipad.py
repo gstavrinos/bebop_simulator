ea04d2086c0ae4d7fe31abcffd0f560005a3b8a1
George Stavrinos <gstavrinos@iit.demokritos.gr>
2017-11-28 01:38:24 +0200

    Optimised the way the moving helipad receives commands, and also added a sleep before starting parameter

--- bebop_gazebo/src/moving_helipad.py
+++ bebop_gazebo/src/moving_helipad.py
@@ -8,20 +8,23 @@ x_vel = 1.0
 y_vel = 0.5
 model_name = ""
 model_state_pub = None
+model_state_sub = None
 
 def init():
-    global model_name, model_state_pub, x_vel, y_vel
+    global model_name, model_state_pub, x_vel, y_vel, model_state_sub
     rospy.init_node('moving_helipad')
     model_name = rospy.get_param("~model_name", "marker3")
     x_vel = rospy.get_param("~x_vel", 1.0)
     y_vel = rospy.get_param("~y_vel", 0.5)
-    rospy.Subscriber("gazebo/model_states", ModelStates, modelStatesCallback)
+    seconds_before_moving = rospy.get_param("~seconds_before_moving", 10)
+    rospy.sleep(seconds_before_moving)
+    model_state_sub = rospy.Subscriber("gazebo/model_states", ModelStates, modelStatesCallback)
     model_state_pub = rospy.Publisher("gazebo/set_model_state", ModelState, queue_size=1)
     while not rospy.is_shutdown():
         rospy.spin()
 
 def modelStatesCallback(msg):
-    global model_name, model_state_pub, x_vel, y_vel
+    global model_name, model_state_pub, x_vel, y_vel, model_state_sub
     index_of_interest = -1
     for i in range(len(msg.name)):
         if msg.name[i] == model_name:
@@ -34,8 +37,9 @@ def modelStatesCallback(msg):
         twist = Twist()
         twist.linear.x = x_vel
         twist.linear.y = y_vel
-        model_state.twist = twist
-        model_state_pub.publish(model_state)
+        if msg.twist[index_of_interest] != twist:
+            model_state.twist = twist
+            model_state_pub.publish(model_state)
 
 if __name__ == '__main__':
     init()
